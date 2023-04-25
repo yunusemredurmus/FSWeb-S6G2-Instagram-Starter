dc188e287a8e1bd38a793ed9a1a001c286ee9c6b
yunusemredurmus <128366167+yunusemredurmus@users.noreply.github.com>
2023-04-26 01:29:59 +0300

    S6G2

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@
 
 // State hook u import edin
 import React from 'react';
-
+import { useState } from 'react';
+import AramaÇubuğu from './bileşenler/AramaÇubuğu/AramaÇubuğu';
+import sahteVeri from './sahte-veri';
+import Gönderiler from './bileşenler/Gönderiler/Gönderiler';
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
 import './App.css';
 
 const App = () => {
+  const [gonderiler, setgonderiler] = useState(sahteVeri);
+
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
+  const [arama, setArama] = useState(AramaÇubuğu)
   const gonderiyiBegen = gonderiID => {
+
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
       Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
 
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
+    "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
       `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
@@ -32,7 +38,9 @@ const App = () => {
 
   return (
     <div className='App'>
-      {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
+      <AramaÇubuğu />
+      <Gönderiler gonderiler={gonderiler} gonderiyiBegen={gonderiyiBegen} />
+
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
   );
